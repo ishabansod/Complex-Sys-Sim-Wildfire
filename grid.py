093071c0a6be7be780121d76ef5f06bf6cd042be
Kwannoo <kwansing2000@hotmail.nl>
2023-06-21 15:40:18 +0200

    load map in grid

--- grid.py
+++ grid.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
 import random
+from PIL import Image
+
 
 class Grid:
     def __init__(self, rows, cols):
@@ -10,10 +12,10 @@ class Grid:
         self.trees = self.init_trees(trees=True)
         self.density = self.init_density(density=True)
         self.altitude = self.init_altitude(altitude=True)
-    
+
     def init_trees(self, trees=False):
         tree_matrix = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
-        
+
         if not trees:  # only one type of tree
             for i in range(self.rows):
                 for j in range(self.cols):
@@ -25,12 +27,12 @@ class Grid:
                         tree_matrix[i][j] = 1
                     else:
                         tree_matrix[i][j] = 2
-        
+
         return tree_matrix
-    
+
     def init_density(self, density):
         den_matrix = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
-        
+
         if not density:  # tree density on the grid
             for i in range(self.rows):
                 for j in range(self.cols):
@@ -42,13 +44,13 @@ class Grid:
                         den_matrix[i][j] = 1
                     else:
                         den_matrix[i][j] = 2
-        
+
         return den_matrix
-    
+
     def init_altitude(self, altitude):
         # TODO see how to incorporate this in burning probability
         alt_matrix = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
-        
+
         if not altitude:  # uniform grid height
             for i in range(self.rows):
                 for j in range(self.cols):
@@ -57,29 +59,70 @@ class Grid:
             for i in range(self.rows):
                 for j in range(self.cols):
                     alt_matrix[i][j] = abs(j - self.cols // 2) + abs(i - self.rows // 2)
-        
+
         return alt_matrix
-    
+
     def colormap(self, title, array):
         np_array = np.array(array)
-        plt.imshow(np_array, interpolation="none", cmap=cm.viridis) # cmap=cm.Reds
+        plt.imshow(np_array, interpolation="none", cmap=cm.viridis)
         plt.title(title)
         plt.show()
-    
+
     def visualize_trees(self):
         self.colormap("Types of Trees Map", self.trees)
-    
+
     def visualize_density(self):
         self.colormap("Density Map", self.density)
-    
+
     def visualize_altitude(self):
         self.colormap("Altitude Map", self.altitude)
-    
+
     def init_grid(self):
-        # states: 1- empty; 2- tree; 3- burning; 4- burnt
+        # states: 1- empty; 2- tree; 3- burning; 4- burnt 5-house
         # forest = [[2 for _ in range(self.cols)] for _ in range(self.rows)]
         grid_density = 0.3
         forest = [[1 if random.random() < grid_density else 2 for _ in range(self.cols)] for _ in range(self.rows)]
+        start_fire_x = self.cols // 2
+        start_fire_y = self.rows // 2
+
+        for row in range(start_fire_y - 1, start_fire_y + 2):
+            for col in range(start_fire_x, start_fire_x + 2):
+                forest[row][col] = 3  # make the center of the grid and neighbors burning
+
+        # house coordinate
+        forest[int(self.rows * 0.8)][int(self.cols * 0.8)] = 5
+
+        return forest
+
+    @staticmethod
+    def load_map(image_path, rows, columns):
+        # Load the image and resize it to the desired dimensions
+        image = Image.open(image_path).resize((rows, columns))
+
+        # Convert the image to RGB mode
+        image_rgb = image.convert("RGB")
+
+        # Define the blue color range
+        blue_color = (157, 217, 243)  # RGB values for "#72cbff"
+        tolerance = 30  # Adjust the tolerance value as needed
+
+        # Create a grid of zeros with the same dimensions as the image
+        grid = np.ones((rows, columns), dtype=int) * 2
+
+        # Iterate over each pixel in the image
+        for i in range(rows):
+            for j in range(columns):
+                # Get the RGB values of the pixel
+                r, g, b = image_rgb.getpixel((i, j))
+
+                # Check if the pixel color falls within the acceptable range
+                if all(abs(channel - blue_channel) <= tolerance for channel, blue_channel in zip((r, g, b), blue_color)):
+                    grid[i, j] = 1
+
+        return grid
+
+    def init_grid_map(self):
+        forest = self.load_map("maps/amsterdam.PNG", self.rows, self.cols)
         start_fire_x = 0
         start_fire_y = self.rows // 2
 
@@ -87,8 +130,11 @@ class Grid:
             for col in range(start_fire_x, start_fire_x + 2):
                 forest[row][col] = 3  # make the center of the grid and neighbors burning
 
+        # house coordinate
+        forest[40][40] = 5
+
         return forest
-    
+
     def burn_trees(self, neighbors):
         # TODO add tree types, density and height, possibly wind too
         # probability of burning
@@ -100,13 +146,14 @@ class Grid:
                 return 3
         return 2
 
-# example
-# rows = 300
-# cols = 300
+    def burn_house(self, neighbors):
+        if any(neighbors[row][col] == 3 for row in range(3) for col in range(3)):
+            return True
 
-# grid = Grid(rows, cols)
-# grid.visualize_trees()
-# grid.visualize_density()
-# grid.visualize_altitude()
+        return False
 
-# forest = grid.init_forest()
+
+# Example usage:
+# grid = Grid(50, 50)
+# forest = grid.init_grid()
+
--- simulate.py
+++ simulate.py
@@ -5,13 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib import animation as animation
+import matplotlib.colors as mcolors
 
 class WildFireSimulation:
     def __init__(self, rows, cols):
         self.grid = Grid(rows, cols)
-        self.current_forest = self.grid.init_grid()
+        self.current_forest = self.grid.init_grid_map()
         self.history = [] # stores a list of forest states
         self.history.append(np.copy(self.current_forest))
+        self.house = False
 
     def update_grid(self):
         # take current grid as input
@@ -21,9 +23,20 @@ class WildFireSimulation:
         # then the cell will be updated according to the rules
 
         state = self.history[-1]
+
+        #random thunderstrike
+
+
         for row in range(1, self.grid.rows - 1):
             for col in range(1, self.grid.cols - 1):
                 
+                thunderstrike_odds = 0.001
+                if random.random() < thunderstrike_odds:
+                    random_row = random.randint(0, self.grid.rows-1)
+                    random_cols = random.randint(0, self.grid.cols-1)
+                    self.current_forest[random_row][random_cols] = 3
+
+
                 # burning cell becomes burnt
                 if state[row][col] == 3:
                     if random.random() < 0.5:
@@ -32,28 +45,54 @@ class WildFireSimulation:
                         self.current_forest[row][col] = 4
 
                 # get neighbors of tree patch, see if it burns or not
-                if state[row][col] == 2:
+                if state[row][col] == 2 or state[row][col] == 5:
                     neighbors = [
                         [state[i][j] for j in range(col - 1, col + 2)] for i in range(row - 1, row + 2)
                         ]
                     # print("-----------------",neighbors)
-                    self.current_forest[row][col] = self.grid.burn_trees(neighbors)
+                    if state[row][col] == 2:
+                        self.current_forest[row][col] = self.grid.burn_trees(neighbors)
+                    else:
+                        self.house = self.grid.burn_house(neighbors)
+
         self.history.append(np.copy(self.current_forest))
         return self.current_forest
     
     def animate(self, steps):
         animations = []
         fig = plt.figure()
+
+        # Define custom colors based on cell values
+        colors = ['white', 'green', 'red', 'orange', 'blue']
+        cmap = mcolors.ListedColormap(colors)
+
         for _ in range(steps):
-            # update_grid()
             new_grid = copy.deepcopy(self.update_grid())
             grid_arr = np.array(new_grid)
 
-            # visualize
-            ani = plt.imshow(grid_arr, animated=True, interpolation="none", cmap=cm.viridis)
+            ani = plt.imshow(grid_arr, animated=True, interpolation="none", cmap=cmap)
             animations.append([ani])
-            
-        gif = animation.ArtistAnimation(fig, animations, interval=100, blit=True,repeat_delay=100)
+
+        gif = animation.ArtistAnimation(fig, animations, interval=100, blit=True, repeat_delay=100)
+        gif.save('gif.gif')
+        plt.show()
+
+    def animate_house(self, steps):
+        animations = []
+        fig = plt.figure()
+
+        # Define custom colors based on cell values
+        colors = ['white', 'green', 'red', 'gray', 'blue']
+        cmap = mcolors.ListedColormap(colors)
+
+        while self.house == False:
+            new_grid = copy.deepcopy(self.update_grid())
+            grid_arr = np.array(new_grid)
+
+            ani = plt.imshow(grid_arr, animated=True, interpolation="none", cmap=cmap)
+            animations.append([ani])
+
+        gif = animation.ArtistAnimation(fig, animations, interval=100, blit=True, repeat_delay=100)
         gif.save('gif.gif')
         plt.show()
 
@@ -92,8 +131,8 @@ class WildFireSimulation:
 
 if __name__=="__main__":
     # Example:
-    rows = 50
-    cols = 50
+    rows = 533
+    cols = 489
     steps = 100
     
     simulation = WildFireSimulation(rows, cols)
