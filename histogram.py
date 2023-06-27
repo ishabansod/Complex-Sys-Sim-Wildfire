586b006dfc36a85863c7c54ad98cad438c4172d1
cyrilrommens <cyrilrommens@gmail.com>
2023-06-27 19:12:29 +0200

    Linear regression on histogram

--- histogram.py
+++ histogram.py
@@ -43,12 +43,30 @@ bins = 10 ** np.linspace(np.log10(MIN), np.log10(MAX), 40)
 frequency, fire_size = plot_loghist2(burned, bins)
 
 # Linear regression
-log_frequency, log_fire_size = np.log10(frequency), np.log10(fire_size)
-slope, intercept = np.polyfit(log_fire_size, log_frequency, 1)
-x_fit = np.linspace(min(log_fire_size), max(log_fire_size), 100)
-y_fit = slope * x_fit + intercept
+# Set min and max index values to fit on the x-axis
+def log_linear_regression(x, y, min_x, max_x):
+    log_y=[0]*len(y)
+    for i in range(0, len(y)):
+        if y[i] != 0:
+            log_y[i] = np.log10(y[i])
+    log_x = np.log10(x)
+    slope, intercept = np.polyfit(log_x[min_x:max_x], log_y[min_x:max_x], 1)
+    x_fit = np.linspace(log_x[min_x], log_x[max_x], len(log_x))
+    y_fit = slope * x_fit + intercept
+
+    # Calculate R_squared
+    residuals = log_y - y_fit
+    total_sum_squares = np.sum((log_y - np.mean(log_y))**2)
+    residual_sum_squares = np.sum(residuals**2)
+    r_squared = 1 - (residual_sum_squares / total_sum_squares)
+
+    return log_x, log_y, x_fit, y_fit, slope, r_squared
+
+log_fire_size, log_frequency, x_fit, y_fit, slope, r_squared = log_linear_regression(fire_size, frequency, 15, 35)
 
 # Plot linear fit
 plt.scatter(log_fire_size, log_frequency)
-#plt.plot(x_fit, y_fit, color='red', label='Linear Fit')
+plt.text(1.6, 1.9, f'R_squared: {r_squared:.2f}', fontsize=12)
+plt.text(1.6, 2, f'Slope: {slope:.2f}', fontsize=12)
+plt.plot(x_fit, y_fit, color='red', label='Linear Fit')
 plt.show()
